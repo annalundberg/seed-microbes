41f9b826f5eff499139ad1c2d813fae76e84f2a4
Anna Lundberg <30848257+annalundberg@users.noreply.github.com>
2019-01-29 20:41:18 -0800

    Update changefastqheader_custom.py

--- python/changefastqheader_custom.py
+++ python/changefastqheader_custom.py
@@ -4,26 +4,23 @@ import sys
 import os
 
 def main():
+    '''Fxn obtains file using sys.argv index. Iterates through fastq file, each header is
+    edited to fit custom format. Each fastq read with an edited header will be written into
+    newfile in folder newheader.'''
     counter = 1
-    fastqfile = open(sys.argv[1], 'r')
-    if not os.path.exists('newheader'):
-        os.makedirs('newheader')
-    newfile = open('newheader/'+fastqfile.name, 'w')
-    for line in fastqfile:
-        if counter > 4:
-            counter = 1
-        if line[0] == '@' and counter == 1:
-            line =  line[:-3]
-            run = line.split("-")[1]
-            line = line.split("-")[0]
-            sample = line.split("_")[0]
-            seq = line.split("_")[1]
-            line = sample+'.'+run+'_'+seq+'\n'
-        newfile.write(line)
-        counter = counter + 1  
-    newfile.close()
-    fastqfile.close()       
-        
-
+    with open(sys.argv[1], 'r') as fastqfile, open('newheader/'+fastqfile.name, 'w') as newfile:
+        if not os.path.exists('newheader'):
+            os.makedirs('newheader')
+        for line in fastqfile:
+            if line[0] == '@' and counter%4 == 1:
+                line =  line[:-3]
+                run = line.split("-")[1]
+                line = line.split("-")[0]
+                sample = line.split("_")[0]
+                seq = line.split("_")[1]
+                line = sample+'.'+run+'_'+seq+'\n'
+            newfile.write(line)
+            counter += 1
+  
 if __name__ == "__main__":
     main()        
